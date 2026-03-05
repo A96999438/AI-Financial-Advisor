3722bd8fac030da0f8f1d1834521d8bd66ac371d
Aman Patel <amanpatel786000@gmail.com>
2026-03-06 00:57:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-import os
-from dotenv import load_dotenv
-import google.generativeai as genai
 import streamlit as st
-
-load_dotenv()
+import google.generativeai as genai
 
 api_key = st.secrets["GEMINI_API_KEY"]
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-pro")
